19a25d84192db422b407ad47fbf9b86f28e6228b
hazem amaar <62269304+hazemamaar@users.noreply.github.com>
2023-04-24 20:04:29 +0200

    android

--- shared/src/commonMain/kotlin/com/example/movieapp/data/remote/KtorApi.kt
+++ shared/src/commonMain/kotlin/com/example/movieapp/data/remote/KtorApi.kt
@@ -8,7 +8,7 @@ import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
 
 private const val BASE_URL ="https://api.themoviedb.org"
-private const val API_KEY =""
+private const val API_KEY ="put your API key here"
 internal abstract class KtorApi {
     val client = HttpClient{
         install(ContentNegotiation){
